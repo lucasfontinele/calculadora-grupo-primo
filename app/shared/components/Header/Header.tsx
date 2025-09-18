12f4c5165ce8f8e2c282feaf729581c2a50a0475
Lucas Fontinele <contato@fontinele.dev>
2025-09-17 23:34:48 -0300

    style: :recycle: changes folder structure

--- app/shared/components/Header/Header.tsx
+++ app/shared/components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import { Grid } from '@/app/shared/components/Grid';
 import Image from 'next/image';
+import Link from 'next/link';
+import { ROUTES } from '@/app/shared/constants/links';
 
 export function Header() {
   return (
     <header className="flex flex-col items-center w-full border-b border-primary-gray">
       <Grid className="py-8">
-        <Image src="/logo.svg" alt="Grupo Primo" width={176} height={32} className="max-md:w-[120px] max-md:h-[22px]" />
+        <Link href={ROUTES.HOME}>
+          <Image
+            src="/logo.svg"
+            alt="Grupo Primo"
+            width={176}
+            height={32}
+            className="max-md:w-[120px] max-md:h-[22px]"
+          />
+        </Link>
       </Grid>
     </header>
   );
