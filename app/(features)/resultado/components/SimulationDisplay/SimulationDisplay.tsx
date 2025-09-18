cd4d3bde12a161357499f6792551aded5945a61c
Lucas Fontinele <contato@fontinele.dev>
2025-09-17 23:31:55 -0300

    feat: :construction: working in progress

--- app/(features)/resultado/components/SimulationDisplay/SimulationDisplay.tsx
+++ app/(features)/resultado/components/SimulationDisplay/SimulationDisplay.tsx
@@ -4,23 +4,23 @@ import Image from 'next/image';
 export function SimulationDisplay() {
   return (
     <div className="w-full">
-      <Grid>
-        <div className="flex flex-col gap-y-12 p-14 max-lg:px-4 max-lg:py-8 bg-tertiary-gray rounded-2xl">
-          <h2 className="text-[40px] font-sans font-bold text-content-primary">
+      <Grid className="max-lg:px-0 max-md:px-0">
+        <div className="flex flex-col gap-y-12 p-14 max-lg:gap-y-8 max-lg:px-4 max-lg:py-8 bg-tertiary-gray rounded-2xl">
+          <h2 className="text-[40px] font-sans font-bold text-content-primary max-lg:text-xl">
             Resultado:
           </h2>
 
-          <div className="flex flex-col gap-y-4 mt-10">
-            <h4 className="font-sans font-semibold text-content-tertiary text-[32px]">
+          <div className="flex flex-col gap-y-4 mt-10 max-lg:mt-0">
+            <h4 className="font-sans font-semibold text-content-tertiary text-[32px] max-lg:text-lg">
               Em 24 meses você teria:
             </h4>
 
             <div className="bg-white rounded-3xl border-border-opaque border p-8 flex flex-col gap-y-4">
-              <small className="uppercase text-base tracking-[5.12px] text-content-secondary font-semibold font-sans">
+              <small className="uppercase text-base tracking-[5.12px] text-content-secondary font-semibold font-sans max-lg:text-sm">
                 Taxa Selic
               </small>
 
-              <strong className="font-semibold font-sans text-[88px] text-content-primary leading-none">
+              <strong className="font-semibold font-sans text-[88px] text-content-primary leading-none max-lg:text-[32px]">
                 R$ 538,00
               </strong>
             </div>
@@ -34,11 +34,11 @@ export function SimulationDisplay() {
                 className="max-lg:max-w-[72px] max-lg:max-h-[20px]"
               />
 
-              <small className="uppercase text-base tracking-[5.12px] text-content-secondary font-semibold font-sans">
+              <small className="uppercase text-base tracking-[5.12px] text-content-secondary font-semibold font-sans max-lg:text-sm">
                 Fundo Arca
               </small>
 
-              <strong className="font-semibold font-sans text-[88px] text-content-primary leading-none">
+              <strong className="font-semibold font-sans text-[88px] text-content-primary leading-none max-lg:text-[32px]">
                 R$ 1.0020,00
               </strong>
             </div>
@@ -57,7 +57,7 @@ export function SimulationDisplay() {
             </div>
 
             <div className="p-4">
-              <span className="flex items-center gap-x-4 uppercase text-base tracking-[5.12px] text-content-secondary font-semibold font-raleway">
+              <span className="flex items-center gap-x-4 uppercase text-base tracking-[5.12px] text-content-secondary font-semibold font-raleway max-lg:flex-col max-lg:items-start max-lg:gap-y-4">
                 RENTABILIDADE DA ARCA:
                 <b className="text-content-primary text-2xl font-open-sans font-extrabold tracking-normal normal-case">
                   18% a.a
@@ -67,17 +67,17 @@ export function SimulationDisplay() {
           </div>
 
           <div className="flex flex-col gap-y-6">
-            <div className="flex items-center gap-x-4">
+            <div className="flex items-center gap-x-4 max-lg:items-start">
               <Image alt="Help" src="/help.svg" width={32} height={32} />
 
-              <span className="font-sans text-lg text-gray">
+              <span className="font-sans text-lg text-content-tertiary max-lg:text-sm">
                 Valores utilizados no simulador de investimentos (referentes à
                 data de última atualização - esses valores podem alterar de
                 acordo com o mercado):
               </span>
             </div>
 
-            <span className="ml-10 font-sans text-lg text-gray">
+            <span className="ml-10 font-sans text-lg text-content-tertiary max-lg:text-sm">
               - Data da última atualização: 10/01/2024
             </span>
           </div>
--- app/(features)/resultado/page.tsx
+++ app/(features)/resultado/page.tsx
@@ -2,7 +2,7 @@ import { SimulationDisplay } from './components/SimulationDisplay/SimulationDisp
 
 export default function ResultPage() {
   return (
-    <div className="flex flex-col w-full px-[120px] py-[80px] max-lg:p-8">
+    <div className="flex flex-col w-full py-[80px] max-lg:p-8">
       <SimulationDisplay />
     </div>
   );
